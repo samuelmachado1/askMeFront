e90f31bf21b735bb27edd73bc772af588082ebd1
Samuel <samuelmachado1@gmail.com>
2024-08-10 23:21:53 -0300

    [aula02] Ajustes de arquitetura

--- src/components/messages.tsx
+++ src/components/messages.tsx
@@ -1,12 +1,13 @@
 import { useParams } from "react-router-dom";
 import { Message } from "./message";
-import { getRoomMessages, GetRoomMessagesResponse } from "../http/get-room-messages";
-import { useQueryClient, useSuspenseQuery } from "@tanstack/react-query";
-import { useEffect } from "react";
+import { getRoomMessages } from "../http/get-room-messages";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { useMessagesWebSockets } from "../hooks/use-messages-websockets";
+
 
 
 export function Messages() {
-  const queryClient = useQueryClient()
+
   const { roomId } = useParams()
 
   if (!roomId) {
@@ -18,91 +19,15 @@ export function Messages() {
     queryFn: () => getRoomMessages({ roomId })
   })
 
+  useMessagesWebSockets({ roomId })
 
-  useEffect(() => {
-    const ws = new WebSocket(`ws://localhost:8080/subscribe/${roomId}`)
-
-    ws.onopen = () => {
-      console.log('Connected websocket!')
-    }
-    ws.onclose = () => {
-      console.log('Disconnected from websocket!')
-    }
-    ws.onmessage = (event) => {
-      const data: {
-        kind: 'message_created' | 'message_answered' | 'reaction_added' | 'reaction_removed',
-        value: any
-      } = JSON.parse(event.data)
-
-      switch (data.kind) {
-        case 'message_created':
-          queryClient.setQueryData<GetRoomMessagesResponse>(['messages', roomId], state => {
-            return {
-              messages: [
-                ...(state?.messages ?? []),
-                {
-                  id: data.value.id,
-                  text: data.value.message,
-                  amountOfReactions: data.value.amountOfReactions,
-                  anserwed: data.value.anserwed,
-                }
-              ]
-            }
-          }
-          )
-          break
-        case 'message_answered':
-          queryClient.setQueryData<GetRoomMessagesResponse>(['messages', roomId], state => {
-            if (!state) {
-              return undefined
-            }
-
-            return {
-              messages: state?.messages?.map(message => {
-                if (message.id === data.value.id) {
-                  return {
-                    ...message,
-                    anserwed: true
-                  }
-                }
-
-                return message
-              })
-            }
-          })
-          console.log('Message answered:', data.value)
-          break
-        case 'reaction_added':
-        case 'reaction_removed':
-          queryClient.setQueryData<GetRoomMessagesResponse>(['messages', roomId], state => {
-            if (!state) {
-              return undefined
-            }
-            return {
-              messages: state.messages.map(message => {
-                if (message.id === data.value.id) {
-                  return {
-                    ...message,
-                    amountOfReactions: data.value.reaction_count
-                  }
-                }
-                return message
-              })
-            }
-          })
-          break
-      }
-    }
-
-    return () => {
-      ws.close()
-    }
-
-  }, [roomId])
+  const sortedMessages = data.messages?.sort((a, b) => {
+    return b.amountOfReactions - a.amountOfReactions
+  })
 
   return (
     <ol className="list-decimal list-outside px-3 space-y-8">
-      {data.messages?.map(message => {
+      {sortedMessages?.map(message => {
         return (
           <Message
             key={message.id}
